a23c82e6cb204ff366b370ce34d229198654b913
Chris McCabe <chris.mccabe@sixdegreelabs.com>
2015-08-09 20:47:00 +0100

    Part 5 - adding ESlint

--- app/user/user.module.js
+++ app/user/user.module.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import { UserCtrl } from './user.ctrl'
-import { UserService } from './user.svc'
+import { UserCtrl } from './user.ctrl';
+import { UserService } from './user.svc';
 
 let controller = UserCtrl;
 let service = UserService.factory;
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 // Require the needed npm packages
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    eslint = require('gulp-eslint');
+
+gulp.task('lint', function () {
+  return gulp.src(['app/**/*.js'])
+    // eslint() attaches the lint output to the eslint property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failOnError last.
+    .pipe(eslint.failOnError());
+});
  
 /**
  * Create a gulp task called html, reload the matching html files
@@ -21,7 +35,7 @@ gulp.task('js', function() {
  */
 gulp.task('watch', function(){
   gulp.watch(['*.html'], ['html']);
-  gulp.watch(['./app/**/*.js'], ['js']);
+  gulp.watch(['./app/**/*.js'], ['js', 'lint']);
 });
 
 /**
@@ -38,4 +52,4 @@ gulp.task('connect', function() {
  * The default gulp task will create the connect server and watch the source
  * files for changes
  */
-gulp.task('default', ['connect','watch']);
+gulp.task('default', ['connect','watch', 'lint']);
